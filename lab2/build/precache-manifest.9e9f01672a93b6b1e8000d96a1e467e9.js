281d57f1b476458bb1053637468b5add30339aad
Ilya_Babich <bilyha.ilya@gmail.com>
2019-01-09 00:50:23 +0300

    commit

---
+++ lab2/build/precache-manifest.9e9f01672a93b6b1e8000d96a1e467e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "203f7c68ded4210eaa3c",
+    "url": "/static/js/main.203f7c68.chunk.js"
+  },
+  {
+    "revision": "a06f66cab165dfc260dd",
+    "url": "/static/js/1.a06f66ca.chunk.js"
+  },
+  {
+    "revision": "203f7c68ded4210eaa3c",
+    "url": "/static/css/main.b50a767d.chunk.css"
+  },
+  {
+    "revision": "88d8fd332033518ebc4683186c6b3baf",
+    "url": "/index.html"
+  }
+];
